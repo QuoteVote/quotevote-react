789bdef6dec76a0ad74b6a655fcee73791d1494f
johhonn <jjj.may377@gmail.com>
2020-04-12 11:18:15 -0400

    upadate for merge

---
+++ src/graphql/query.js
@@ -0,0 +1,33 @@
+import gql from "graphql-tag";
+
+export const DOMAIN_QUERY = gql`
+query domains($limit: Int!) {
+  domains(limit: $limit) {
+    _id
+    adminIds
+    allowedUserIds
+    key
+    privacy
+    title
+    url
+    description
+  }
+}
+`
+export const USER_INVITE_REQUESTS = gql`
+    query userInviteRequests {
+      userInviteRequests {
+        email
+        status
+        _id
+      }
+    }
+
+
+`
+
+export const UPDATE_USER_INVITE_STATUS = gql`
+  mutation sendUserInviteApproval($user_invite_id: String!, $action: String!) {
+    sendUserInviteApproval(user_invite_id: $user_invite_id, action: $action)
+  }
+`;
