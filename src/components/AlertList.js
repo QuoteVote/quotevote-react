cb205e315b92bb38db444dfd1a99aa3c3a4bad7e
mzpolini <matthew.polini@gmail.com>
2020-06-23 12:34:13 -0400

    Applying style v2 to the Activity Cards
    I refactored the existing activity cards to more closely model the new
    mockup cards.
    
    Adds
    	-  MUI GridList & GridListTile to handle display of cards
    		*Decisions can be made about how many columns of cards should
    		be displayed per device width, currently | xl=4 lg=4 md=3 sm=2 xs=1
    
    Changes
    	-  Removed inline styling and build classes in makeStyles()
    
    Todo
    	-  Get spacing and font types PERFECT

--- src/components/AlertList.js
+++ src/components/AlertList.js
@@ -1,8 +1,26 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+import { useTheme } from '@material-ui/core/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import Skeleton from '@material-ui/lab/Skeleton'
-import GridContainer from 'mui-pro/Grid/GridContainer'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
 import Alert from './Alert'
+
+const getGridListCols = (width) => {
+  switch (width) {
+    case 'xl':
+      return 4
+    case 'lg':
+      return 4
+    case 'md':
+      return 3
+    case 'sm':
+      return 2
+    default:
+      return 1
+  }
+}
 
 function AlertSkeletonLoader({ limit }) {
   const rows = Array.from(Array(limit).keys())
@@ -20,40 +38,49 @@ function AlertSkeletonLoader({ limit }) {
   )
 }
 
-function LoadAlertList({ data }) {
+function useWidth() {
+  const theme = useTheme()
+  const keys = [...theme.breakpoints.keys].reverse()
+  return (
+    keys.reduce((output, key) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const matches = useMediaQuery(theme.breakpoints.up(key))
+      return !output && matches ? key : output
+    }, null) || 'xs'
+  )
+}
+
+function LoadAlertList({ data, width }) {
   if (!data || data.length === 0) {
     return (
       <div style={{ width: '90%' }}>
-        <span>Hmmmm, we&apos;re not getting any results. Our bad - try another search.</span>
+        <span>Hmmmm, we&aposre not getting any results. Our bad - try another search.</span>
         <br></br>
       </div>
     )
   }
 
-  return data.map((item) => (
-    <div style={{ width: '90%' }}>
-      <Alert
-        color={item.color}
-        AlertTitle={item.AlertTitle}
-        AlertBody={item.AlertBody}
-        time={item.time}
-        points={item.points}
-        creator={item.creator}
-      />
-      <br></br>
-    </div>
-  ))
+  return (
+    <GridList cols={getGridListCols(width)}>
+      {data.map((item) => (
+        <GridListTile key={item.AlertTitle} cols={1}>
+          <Alert
+            color={item.color}
+            AlertTitle={item.AlertTitle}
+            AlertBody={item.AlertBody}
+            time={item.time}
+            points={item.points}
+            creator={item.creator}
+          />
+        </GridListTile>
+      ))}
+    </GridList>
+  )
 }
 
 
 export default function AlertList({ Data, loading, limit }) {
-  return (
-    <GridContainer
-      direction="column"
-      justify="space-between"
-      alignItems="center"
-    >
-      {loading ? (<AlertSkeletonLoader limit={limit} />) : <LoadAlertList data={Data} />}
-    </GridContainer>
-  )
+  const width = useWidth()
+  if (loading) return <AlertSkeletonLoader limit={limit} />
+  return <LoadAlertList data={Data} width={width} />
 }
