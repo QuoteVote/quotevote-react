84e2a8c3f86fd606d1b80d915ce92bf089465f10
Jovan Cahiles <jovancahiles@gmail.com>
2020-08-27 04:13:48 +0800

    initial tests

--- src/components/Navbars/MainNavBar.js
+++ src/components/Navbars/MainNavBar.js
@@ -1,39 +1,37 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import PropTypes from 'prop-types'
-import Grid from '@material-ui/core/Grid'
-import Hidden from '@material-ui/core/Hidden'
-import Tabs from '@material-ui/core/Tabs'
-import { NavLink } from 'react-router-dom'
-import Tab from '@material-ui/core/Tab'
-import SvgIcon from '@material-ui/core/SvgIcon'
-import IconButton from '@material-ui/core/IconButton'
-import { Typography } from '@material-ui/core'
-import AppBar from '@material-ui/core/AppBar'
-import Avatar from '@material-ui/core/Avatar'
-import withWidth from '@material-ui/core/withWidth'
-import { SET_SELECTED_PAGE } from 'store/ui'
-import { ReactComponent as HomeSvg } from '../../assets/svg/Home.svg'
-import { ReactComponent as TrendingSvg } from '../../assets/svg/TrendingIcon.svg'
-import { ReactComponent as AddPostSvg } from '../../assets/svg/AddPost.svg'
-import { ReactComponent as ChatSvg } from '../../assets/svg/Chat.svg'
-import { ReactComponent as NotificationsSvg } from '../../assets/svg/Notifications.svg'
-import { ReactComponent as SettingsSvg } from '../../assets/svg/Settings.svg'
-import voxPopIcon from '../../assets/img/voxPopIcon.jpg'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PropTypes from "prop-types";
+import Grid from "@material-ui/core/Grid";
+import Hidden from "@material-ui/core/Hidden";
+import Tabs from "@material-ui/core/Tabs";
+import { NavLink } from "react-router-dom";
+import Tab from "@material-ui/core/Tab";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import IconButton from "@material-ui/core/IconButton";
+import { Typography } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Avatar from "@material-ui/core/Avatar";
+import withWidth from "@material-ui/core/withWidth";
+import { SET_SELECTED_PAGE } from "store/ui";
+import { ReactComponent as HomeSvg } from "../../assets/svg/Home.svg";
+import { ReactComponent as TrendingSvg } from "../../assets/svg/TrendingIcon.svg";
+import { ReactComponent as AddPostSvg } from "../../assets/svg/AddPost.svg";
+import { ReactComponent as ChatSvg } from "../../assets/svg/Chat.svg";
+import { ReactComponent as NotificationsSvg } from "../../assets/svg/Notifications.svg";
+import { ReactComponent as SettingsSvg } from "../../assets/svg/Settings.svg";
+import voxPopIcon from "../../assets/img/voxPopIcon.jpg";
 
 function MainNavBar(props) {
-  const {
-    classes, setChatOpen, chatOpen, width,
-  } = props
-  const selectedPage = useSelector((state) => state.ui.selectedPage)
-  const username = useSelector((state) => state.user.data.username)
-  const avatar = useSelector((state) => state.user.data.avatar)
-  const name = useSelector((state) => state.user.data.name)
-  const fontSize = width === 'md' ? 'medium' : 'large'
-  const dispatch = useDispatch()
+  const { classes, setChatOpen, chatOpen, width } = props;
+  const selectedPage = useSelector((state) => state.ui.selectedPage);
+  const username = useSelector((state) => state.user.data.username);
+  const avatar = useSelector((state) => state.user.data.avatar);
+  const name = useSelector((state) => state.user.data.name);
+  const fontSize = width === "md" ? "medium" : "large";
+  const dispatch = useDispatch();
   const handleMenu = (newSelectedMenu) => {
-    dispatch(SET_SELECTED_PAGE(newSelectedMenu))
-  }
+    dispatch(SET_SELECTED_PAGE(newSelectedMenu));
+  };
   return (
     <AppBar position="fixed" className={classes.appBar}>
       <Grid
@@ -47,12 +45,7 @@ function MainNavBar(props) {
           <img alt="voxPOP" src={voxPopIcon} className={classes.voxPop} />
         </Grid>
 
-        <Grid
-          container
-          item
-          lg={5}
-          justify="flex-start"
-        >
+        <Grid container item lg={5} justify="flex-start">
           <Tabs
             value={selectedPage}
             onChange={handleMenu}
@@ -62,12 +55,16 @@ function MainNavBar(props) {
             <Grid item lg={4}>
               <NavLink to="/hhsb/Home">
                 <Tab
-                  icon={(
-                    <SvgIcon component={HomeSvg} fontSize={fontSize} viewBox="0 0 37 37" />
-                  )}
+                  icon={
+                    <SvgIcon
+                      component={HomeSvg}
+                      fontSize={fontSize}
+                      viewBox="0 0 37 37"
+                    />
+                  }
                   aria-label="Home"
                   onClick={() => {
-                    handleMenu(0)
+                    handleMenu(0);
                   }}
                   wrapped
                   value="home"
@@ -77,34 +74,34 @@ function MainNavBar(props) {
             <Grid item lg={4}>
               <NavLink to="/hhsb/TrendingContent">
                 <Tab
-                  icon={(
+                  icon={
                     <SvgIcon
                       component={TrendingSvg}
                       fontSize={fontSize}
                       viewBox="0 0 50 50"
                     />
-                  )}
+                  }
                   aria-label="Trending"
                   onClick={() => {
-                    handleMenu(1)
+                    handleMenu(1);
                   }}
                   value="trending"
                 />
               </NavLink>
             </Grid>
             <Grid item lg={4}>
-              <NavLink to="/hhsb/SubmitPost">
+              <NavLink id="submit-post-button" to="/hhsb/SubmitPost">
                 <Tab
-                  icon={(
+                  icon={
                     <SvgIcon
                       component={AddPostSvg}
                       fontSize={fontSize}
                       viewBox="0 0 32 32"
                     />
-                  )}
+                  }
                   aria-label="Post"
                   onClick={() => {
-                    handleMenu(2)
+                    handleMenu(2);
                   }}
                   value="post"
                 />
@@ -113,22 +110,13 @@ function MainNavBar(props) {
           </Tabs>
         </Grid>
 
-        <Grid
-          justify="flex-end"
-          container
-          item
-          alignItems="center"
-          lg={5}
-        >
+        <Grid justify="flex-end" container item alignItems="center" lg={5}>
           <Grid item lg={2}>
-            <IconButton
-              aria-label="Profile"
-              color="inherit"
-            >
+            <IconButton aria-label="Profile" color="inherit">
               <Avatar alt={username} src={avatar} />
             </IconButton>
           </Grid>
-          <Hidden only={['md']}>
+          <Hidden only={["md"]}>
             <Grid item lg={4}>
               <Typography variant="h6" className={classes.profileBlockName}>
                 {name}
@@ -179,7 +167,7 @@ function MainNavBar(props) {
         </Grid>
       </Grid>
     </AppBar>
-  )
+  );
 }
 
 MainNavBar.propTypes = {
@@ -187,6 +175,6 @@ MainNavBar.propTypes = {
   setChatOpen: PropTypes.func.isRequired,
   chatOpen: PropTypes.bool.isRequired,
   width: PropTypes.string.isRequired,
-}
+};
 
-export default withWidth()(MainNavBar)
+export default withWidth()(MainNavBar);
