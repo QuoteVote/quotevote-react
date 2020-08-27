4e04893197ff60bb8a0003c3f061b3f82611ff6c
mzpolini <matthew.polini@gmail.com>
2020-08-26 23:46:05 -0400

    lint date search bar and profile header

--- src/components/DateSearchBar.js
+++ src/components/DateSearchBar.js
@@ -1,41 +1,14 @@
 import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import { makeStyles } from '@material-ui/core/styles'
-import { useQuery } from '@apollo/react-hooks'
+// import PropTypes from 'prop-types'
+// import { makeStyles } from '@material-ui/core/styles'
 import gql from 'graphql-tag'
 import 'react-dates/initialize'
 import 'react-dates/lib/css/_datepicker.css'
 import { DateRangePicker } from 'react-dates'
-// import Paper from '@material-ui/core/Paper'
-// import InputBase from '@material-ui/core/InputBase'
-// import Divider from '@material-ui/core/Divider'
-// import IconButton from '@material-ui/core/IconButton'
-// import SearchIcon from '@material-ui/icons/Search'
-// import TextField from '@material-ui/core/TextField'
-// import { DateRangePicker, DateRange, DateRangeDelimiter } from '@material-ui/pickers'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    flexBasis: 300,
-    minWidth: 0,
-    flexWrap: 'inherit',
-    maxHeight: 50,
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    height: 28,
-    margin: 4,
-  },
-}))
+//  Todo - styling
+// const useStyles = makeStyles((theme) => ({
+// }))
 
 export const GET_SEARCH_START_DATE = gql`
   {
@@ -43,9 +16,10 @@ export const GET_SEARCH_START_DATE = gql`
   }
 `
 
-DateSearchBar.propTypes = {
-  setOffset: PropTypes.func.isRequired,
+Datepicker.propTypes = {
+  // setOffset: PropTypes.func.isRequired,
 }
+
 export default function Datepicker() {
   const [dateRange, setDateRange] = useState({
     startDate: null,
@@ -75,48 +49,6 @@ export default function Datepicker() {
       startDateId="startDateMookh"
       endDateId="endDateMookh"
       minimumNights={0}
-    />
-  )
-}
-
-
-function DateSearchBar({ setOffset }) {
-  // eslint-disable-next-line no-unused-vars
-  const classes = useStyles()
-  const { client } = useQuery(GET_SEARCH_START_DATE)
-  // const [selectedDate, handleDateChange] = React.useState<DateRange>([null, null])
-  // eslint-disable-next-line no-unused-vars
-  const handleChangeStartDate = (event) => {
-    client.writeData({ data: { startDateRange: event.target.value } })
-    setOffset(0)
-  }
-
-  // console.log('Rendering DateSearchBar startDateRange', startDateRange)
-  /*
-  In an ideal world we can use this
-  <DateRangePicker
-    startText="Check-in"
-    endText="Check-out"
-    value={selectedDate}
-    onChange={(date) => handleDateChange(date)}
-    renderInput={(startProps, endProps) => (
-      <>
-        <TextField {...startProps} />
-        <DateRangeDelimiter> to </DateRangeDelimiter>
-        <TextField {...endProps} />
-      </>
-    )}
-  />
-  */
-  return (
-    <DateRangePicker
-      startDate={null} // momentPropTypes.momentObj or null,
-      startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-      endDate={null} // momentPropTypes.momentObj or null,
-      endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-      onDatesChange={() => {}} // PropTypes.func.isRequired,
-      focusedInput={null} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-      onFocusChange={() => {}} // PropTypes.func.isRequired,
     />
   )
 }
--- src/components/Navbars/ProfileHeader.js
+++ src/components/Navbars/ProfileHeader.js
@@ -67,27 +67,6 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '50%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-  headerToggle: {
-    // fontSize: '20px',
-    // color: 'gray',
-    // font: 'League Spartan',
-    // fontWeight: '900',
-    // textDecoration: 'underline',
-    // textShadow: '1px 1px gray',
-  },
   sectionDesktop: {
     display: 'none',
     [theme.breakpoints.up('md')]: {
