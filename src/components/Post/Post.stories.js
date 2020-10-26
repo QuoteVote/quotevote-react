88d7494eec85178a02f9c989f1ced676fb82aa86
olivermolina <olivermolina10@gmail.com>
2020-10-26 19:38:14 +0800

    Fix storybook error

--- src/components/Post/Post.stories.js
+++ src/components/Post/Post.stories.js
@@ -17,18 +17,25 @@ export default {
 }
 
 const post = {
-  user: {
+  creator: {
     name: 'John Doe',
+    avatar: 'J',
   },
   created: '11/06/2018 11:00 AM',
   title: 'Title of a post',
   text: 'What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
   upvotes: 100,
   downvotes: 200,
+  approvedBy: [],
+  rejectedBy: [],
+}
+
+const user = {
+  name: 'John Doe',
   avatar: 'J',
 }
 
-export const Base = () => <Post post={post} />
+export const Base = () => <Post post={post} user={user} />
 export const Loading = () => <PostSkeleton />
 
 Base.story = {
