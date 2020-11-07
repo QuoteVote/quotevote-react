0e91a78659e7b9148fb57d8246973e976fab3365
isaac <neoisaac.amao@gmail.com>
2020-11-07 13:25:19 +0800

    fix lint error

--- src/components/PostActions/PostActionList.js
+++ src/components/PostActions/PostActionList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {
   Card, CardContent, Grid, GridList, GridListTile, IconButton,
 } from '@material-ui/core'
-import { isEmpty } from 'lodash' 
+import { isEmpty } from 'lodash'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 import { Skeleton } from '@material-ui/lab'
