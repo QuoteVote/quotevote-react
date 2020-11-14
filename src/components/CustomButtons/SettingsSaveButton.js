67d6fdd98e741f4ca78b8faa17635a5154bd6a11
olivermolina <olivermolina10@gmail.com>
2020-11-14 14:02:17 +0800

    Settings

---
+++ src/components/CustomButtons/SettingsSaveButton.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+
+const useStyles = makeStyles(() => ({
+  button: {
+    fontFamily: 'Roboto',
+    fontSize: '14px',
+    fontWeight: 'normal',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: 'normal',
+    letterSpacing: 'normal',
+    textAlign: 'right',
+    color: '#ffffff',
+  },
+}))
+
+function SettingsSaveButton(props) {
+  const classes = useStyles()
+
+  return (
+    <Button
+      {...props}
+      className={classNames(classes.button, props.className)}
+      type="submit"
+    >
+      Save changes
+    </Button>
+  )
+}
+
+SettingsSaveButton.propTypes = {
+  className: PropTypes.any,
+}
+
+export default SettingsSaveButton
---
+++ src/components/CustomButtons/SignOutButton.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+
+const useStyles = makeStyles(() => ({
+  button: {
+    fontFamily: 'Roboto',
+    fontSize: '14px',
+    fontWeight: 'normal',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: 'normal',
+    letterSpacing: 'normal',
+    textAlign: 'right',
+    color: '#ffffff',
+  },
+}))
+
+function SignOutButton(props) {
+  const classes = useStyles()
+
+  return (
+    <Button
+      {...props}
+      className={classNames(classes.button, props.className)}
+    >
+      Sign out
+    </Button>
+  )
+}
+
+SignOutButton.propTypes = {
+  className: PropTypes.any,
+}
+
+export default SignOutButton
