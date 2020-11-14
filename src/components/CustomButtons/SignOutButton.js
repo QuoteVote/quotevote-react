28a1e893a26a058403214662b8d5d0135f390e58
olivermolina <olivermolina10@gmail.com>
2020-11-14 14:14:14 +0800

    Added logout icon

--- src/components/CustomButtons/SignOutButton.js
+++ src/components/CustomButtons/SignOutButton.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
-
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 const useStyles = makeStyles(() => ({
   button: {
     fontFamily: 'Roboto',
@@ -25,6 +25,7 @@ function SignOutButton(props) {
     <Button
       {...props}
       className={classNames(classes.button, props.className)}
+      startIcon={<ExitToAppIcon />}
     >
       Sign out
     </Button>
