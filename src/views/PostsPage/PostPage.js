962df80125b4537dc7834c146eb0a2378638e9e0
Sarah Naas <sjnaas@gmail.com>
2021-02-09 16:44:30 -0800

    got both queries working

--- src/views/PostsPage/PostPage.js
+++ src/views/PostsPage/PostPage.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react'
 import { Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useSubscription } from '@apollo/react-hooks'
 import { useSelector } from 'react-redux'
 import { isEmpty } from 'lodash'
 import Post from '../../components/Post/Post'
 import PostActionList from '../../components/PostActions/PostActionList'
-import { GET_POST } from '../../graphql/query'
 import PostSkeleton from '../../components/Post/PostSkeleton'
 import PostChat from '../../components/PostChat/PostChat'
+import { GET_ROOM_MESSAGES, GET_POST } from '../../graphql/query'
+import { NEW_MESSAGE_SUBSCRIPTION } from '../../graphql/subscription'
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -25,15 +26,16 @@ function PostPage() {
   }, [])
 
   const postId = useSelector((state) => state.ui.selectedPost.id)
-  const { loading, error, data } = useQuery(GET_POST, {
+
+  const { loading: loadingPost, error: postError, data: postData } = useQuery(GET_POST, {
     variables: { postId },
     fetchPolicy: 'cache-and-network',
   })
 
   const user = useSelector((state) => state.user.data)
 
-  if (error) return 'Something went wrong!'
-  const { post } = !loading && data
+  if (postError) return 'Something went wrong!'
+  const { post } = !loadingPost && postData
   console.log(post)
 
   let messageRoomId
@@ -42,10 +44,34 @@ function PostPage() {
     title = post.title
     messageRoomId = post.messageRoom._id
   }
+
+  const {
+    loading: loadingMessages, error: messageError, data: messageData, refetch,
+  } = useQuery(GET_ROOM_MESSAGES, {
+    variables: { messageRoomId },
+  })
+
+  console.log(messageData)
+
+  useSubscription(
+    NEW_MESSAGE_SUBSCRIPTION,
+    {
+      variables: { messageRoomId },
+      onSubscriptionData: async () => {
+        await refetch()
+      },
+    },
+  )
+
   const {
     comments, votes, quotes, url,
   } = post || { comments: [], votes: [], quotes: [] }
   let postActions = []
+
+  //add messages here
+  // get messages associated with the messageRoomId
+  //if messages exist - copy display them with the rest of the data. 
+  //If not, yada yada
 
   if (!isEmpty(comments)) {
     postActions = postActions.concat(comments)
@@ -69,11 +95,11 @@ function PostPage() {
       style={{ position: 'relative' }}
     >
       <Grid item xs={12} md={6}>
-        {loading ? <PostSkeleton /> : <Post post={post} loading={loading} user={user} />}
+        {loadingPost ? <PostSkeleton /> : <Post post={post} loading={loadingPost} user={user} />}
       </Grid>
-      <Grid item direction="column" className={classes.root} xs={12} md={6}>
+      <Grid item className={classes.root} xs={12} md={6}>
         <PostChat messageRoomId={messageRoomId} title={title} />
-        <PostActionList loading={loading} postActions={postActions} postUrl={url} />
+        <PostActionList loading={loadingPost} postActions={postActions} postUrl={url} />
       </Grid>
     </Grid>
   )
