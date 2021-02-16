3b44bc3a5f5433f18f704e66f18b6ab88b1a2f50
Sarah Naas <sjnaas@gmail.com>
2021-02-15 18:17:07 -0800

    made post scrollable

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -21,6 +21,10 @@ import AvatarDisplay from '../Avatar'
 import BookmarkIconButton from '../CustomButtons/BookmarkIconButton'
 
 const useStyles = makeStyles(() => ({
+  root: {
+    maxHeight: '720px',
+    overflowY: 'scroll',
+  },
   header2: {
     padding: 0,
     marginLeft: 10,
