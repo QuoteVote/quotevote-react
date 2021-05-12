ee60e44152147b84f42db53a544bdba0a5bc4813
Sarah Naas <sjnaas@gmail.com>
2021-05-12 13:31:04 -0700

    removed second instance of hhsb

--- src/layouts/Scoreboard.js
+++ src/layouts/Scoreboard.js
@@ -85,9 +85,6 @@ function Scoreboard(props) {
     } = props
     const currLocation = pathname.split('/')
 
-    // const currentPage = appRoutes.filter(
-    //   (appRoute) => appRoute.layout === `/${currLocation[1]}` && appRoute.path === `/${currLocation[2]}`,
-    // )
     let currentPage
     if (pathname.includes('/auth') || pathname.includes('/logout') || pathname.includes('/error')) {
       currentPage = appRoutes.filter(
@@ -98,8 +95,6 @@ function Scoreboard(props) {
         (appRoute) => appRoute.layout === '/' && appRoute.path === `${currLocation[1]}`,
       )
     }
-
-    console.log(currentPage)
 
     setPage(currentPage[0].name)
   }, [props])
